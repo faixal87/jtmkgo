2d022971da6af7db50f3fb1096e5ccd6561d8b7b
Faizal <m.faizal.yahaya@gmail.com>
2026-07-15 00:41:25 +0800

    Refine birthday email recipient wording

--- app/Mail/BirthdayNotificationMail.php
+++ app/Mail/BirthdayNotificationMail.php
@@ -19,13 +19,42 @@ class BirthdayNotificationMail extends Mailable
 
     public function build(): self
     {
+        $displayName = $this->displayName();
+
         return $this
-            ->subject("Happy Birthday, {$this->recipient->name}!")
+            ->subject("Happy Birthday, {$displayName}!")
             ->view('emails.birthday-notification')
             ->with([
+                'displayName' => $displayName,
                 'photoPath' => $this->recipient->profilePhotoStoragePath(),
-                'initials' => $this->recipient->initials() ?: 'J',
-                'actionLabel' => $this->actionLabel ?: 'View Profile',
+                'initials' => $this->initialsFrom($displayName),
             ]);
+    }
+
+    private function displayName(): string
+    {
+        $name = trim((string) preg_replace('/\s+/', ' ', $this->recipient->name));
+
+        $name = (string) preg_replace(
+            '/^(?:(?:PROF\.?\s*MADYA|ASSOC\.?\s*PROF|TS|IR|DR|PROF|DATUK|DATO|TUAN|PUAN|ENCIK|CIK)\.?\s+)+/i',
+            '',
+            $name
+        );
+
+        $name = (string) preg_replace('/\s+(?:BIN|BINTI|BT|BTE|B\.)\s+.*$/i', '', $name);
+        $name = trim($name);
+
+        return $name !== '' ? $name : $this->recipient->name;
+    }
+
+    private function initialsFrom(string $name): string
+    {
+        $initials = collect(explode(' ', trim($name)))
+            ->filter()
+            ->take(2)
+            ->map(fn (string $part): string => strtoupper(substr($part, 0, 1)))
+            ->implode('');
+
+        return $initials !== '' ? $initials : 'J';
     }
 }
